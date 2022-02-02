20bb915a339d9b012e5fe24233655cd45d1b9a13
traaga <traaga>
2022-02-02 22:52:45 +0200

    Change basename to PUBLIC_URL

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         <GlobalStateProvider>
             <StyledEngineProvider injectFirst>
                 <ThemeProvider theme={theme}>
-                    <BrowserRouter basename={window.location.pathname || ''}>
+                    <BrowserRouter basename={process.env.PUBLIC_URL}>
                         <Routes>
                             <Route path="/">
                                 <Route index element={<Home />} />
